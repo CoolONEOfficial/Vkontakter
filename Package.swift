c61c36d60a928a449d4327d33aed5f220047a597
coolone <coolone.official@gmail.com>
2021-04-01 01:15:25 +0300

    Revert "platform macos"
    This reverts commit 8a3925baf33238b35276ee3c0b10d7faf910597f.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Vkontakter",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "Vkontakter", targets: ["Vkontakter"]),
